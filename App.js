90a1b298fe87f92761f164e001477a43b3540b00
Jaindivyansh05 <92245520+Jaindivyansh05@users.noreply.github.com>
2025-07-30 14:35:52 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dcards from "./Dcards";
-import BookingForm from "./BookingForm";
-import Navbar from "./Navbar";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import Dcards from "./Components/Dcards";
+import BookingForm from "./Components/BookingForm";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div className="App">
-        <Navbar />
+        <NavBar />
         <Routes>
+          {/* Redirect from "/" to "/locations" */}
+          <Route path="/" element={<Navigate to="/locations" />} />
           <Route path="/locations" element={<Dcards />} />
           <Route path="/bookVisit/:locationName" element={<BookingForm />} />
         </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
